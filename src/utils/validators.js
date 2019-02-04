1976b3ae1a27c47dca6c54e7d75cf726e9b8a0dc
Xelp <xelp@14.143.125.162.static-Bangalore.vsnl.net.in>
2019-02-04 12:00:10 +0530

    unused variables has been removed

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,15 +1,16 @@
 export function isEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return !re.test(String(email).toLowerCase());
+  // eslint-disable-next-line
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return !re.test(String(email).toLowerCase());
 }
 
 export function noNumbers(value) {
-    return /\d/.test(value);
+  return /\d/.test(value);
 }
 
 export function isEmpty(value) {
-    if (!value) {
-        return false;
-    }
-    return value.length === 0;
+  if (!value) {
+    return false;
+  }
+  return value.length === 0;
 }
